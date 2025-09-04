029839962df1d1664ae0b247333e65c5e33ca086
brampeirs <bram.peirs@showpad.com>
2025-09-04 17:03:19 +0200

    fix cors, only allow vercel app

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # tijdelijk: alles toestaan
+    allow_origins=["https://finance-app-three-rosy.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
